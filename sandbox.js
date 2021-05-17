6eb491f409f8fbc6be083201f4a5fdf1aed1b7e9
jerrycode06 <nikhil9690@gmail.com>
2021-05-17 06:23:28 +0530

    type checking

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var circles = function (radius) {
+    return radius * radius * Math.PI;
+};
+// console.log(circles("Hello"))
+console.log(circles(5));
+// Arrays
---
+++ sandbox.ts
@@ -0,0 +1,8 @@
+const circles = (radius: number) => {
+  return radius * radius * Math.PI;
+};
+
+// console.log(circles("Hello"))
+console.log(circles(5));
+
+// Arrays
