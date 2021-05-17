db3fafa9ce97b387bedd49254b4d2e1556cf5def
jerrycode06 <nikhil9690@gmail.com>
2021-05-17 07:09:56 +0530

    arrays and objects

--- sandbox.ts
+++ sandbox.ts
@@ -6,3 +6,25 @@ const circles = (radius: number) => {
 console.log(circles(5));
 
 // Arrays
+let names = ["Jerry", "Batman", "Flash", "Aquaman"];
+names.push("Wonder Woman");
+// names.push(78); error
+// names[1] = 21; error
+
+// names = "897" --> gives me error because can't change data type of array or objects after declaring
+
+let mixed = ["pizza", 150, "pasta", 116, "coke"];
+mixed.push(20); // will work if you define a mixed array which contails different datatypes previously
+mixed[3] = "burger"; // will work
+
+// Objects
+let shinobi = {
+  name: "itachi",
+  jutsu: "genjutsu",
+  age: 15,
+};
+
+shinobi.age = 25; // gonna work
+shinobi.name = "shishui"; //gonna work
+// shinobi.age = '25' // not gonna work, you can't change value after declaration first time
+// shinobi.skills = [] // not gonna work, you can't add other key-values after declaration
