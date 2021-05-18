185381645574a1c5e3e0ae3f97945c6dcd84164e
jerrycode06 <nikhil9690@gmail.com>
2021-05-18 17:50:39 +0530

    oops tsc 2

--- public/app.js
+++ public/app.js
@@ -1,11 +1,20 @@
 "use strict";
 // Classes
 var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
+    //   readonly client: string; // can access outside the class but you'll not change the value
+    //   public details: string; // can access or can change the value outside the class
+    //   private amount: number; //can't access outside the class but can access through class method
+    //   constructor(c: string, d: string, a: number) {
+    //     this.client = c;
+    //     this.details = d;
+    //     this.amount = a;
+    //   }
+    // A short way to do all things specified above
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    } // value will be assigned automatically
     Invoice.prototype.format = function () {
         return this.client + " owes " + this.amount + " for " + this.details;
     };
@@ -21,7 +30,7 @@ invoices.push(invOne);
 invoices.push(invTwo);
 // console.log(invoices);
 invoices.forEach(function (inv) {
-    console.log(inv.client, inv.details, inv.amount, inv.format());
+    console.log(inv.client, inv.details, inv.format());
 });
 var anchor = document.querySelector("a");
 // sometime it gives warning like anchor may be undefined so we will do some checks like put ! after querySelector
